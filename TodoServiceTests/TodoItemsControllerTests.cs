5b846234737de67d007c5799baaa8c30ee55afb7
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-14 21:45:44 -0600

    Added test project

---
+++ TodoServiceTests/TodoItemsControllerTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TodoServiceTests
+{
+    public class TodoItemsControllerTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
