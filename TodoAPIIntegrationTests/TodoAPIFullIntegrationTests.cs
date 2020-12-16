08c20b9b8815742dea304def655064a9667ff59a
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-16 15:44:59 -0600

    Added test for getTodo

--- TodoAPIIntegrationTests/TodoAPIFullIntegrationTests.cs
+++ TodoAPIIntegrationTests/TodoAPIFullIntegrationTests.cs
@@ -31,8 +31,23 @@ namespace TodoAPIIntegrationTests
             httpResponse.EnsureSuccessStatusCode();
             var stringResponse = await httpResponse.Content.ReadAsStringAsync();
             var todos = JsonConvert.DeserializeObject<IEnumerable<Todo>>(stringResponse);
-            Assert.Contains(todos, p => p.Name == "Feed the dog");
+            Assert.Contains(todos, t => t.Name == "Feed the dog");
             Assert.Equal(3, todos.Count());
+        }
+
+        [Fact]
+        public async Task GetTodo_ReturnsSpecifiedTodo()
+        {
+            // Arrange, See CustomWebApplicationFactory
+
+            // Act
+            var httpResponse = await _client.GetAsync("/api/Todo/1");
+
+            // Assert
+            httpResponse.EnsureSuccessStatusCode();
+            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
+            var todo = JsonConvert.DeserializeObject<Todo>(stringResponse);
+            Assert.Equal(1, todo.Id);
         }
     }
 }
