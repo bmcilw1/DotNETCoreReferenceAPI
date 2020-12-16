bdc837faf33cd5d88bfb2becd91707a7658b5446
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-16 14:44:00 -0600

    Added integrationTests scaffolding

---
+++ TodoAPIIntegrationTests/TodoAPIFullIntegrationTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TodoAPIIntegrationTests
+{
+    public class TodoAPIFullIntegrationTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
