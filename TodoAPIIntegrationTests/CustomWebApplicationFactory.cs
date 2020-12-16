1a7409056a7563031ebaa8b7690c0fc6818dfadb
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-16 15:22:52 -0600

    Fixed web app factory usings

--- TodoAPIIntegrationTests/CustomWebApplicationFactory.cs
+++ TodoAPIIntegrationTests/CustomWebApplicationFactory.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Web.Api.Infrastructure.Data;
+using TodoAPI.Repositories;
 
 namespace TodoAPIIntegrationTests
 {
