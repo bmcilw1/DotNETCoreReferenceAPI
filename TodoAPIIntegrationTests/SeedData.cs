e0b8c7d39e97a85293bf2b2367aca62d6d38876f
Brian McIlwain <technicallyemployed@gmail.com>
2020-12-16 15:14:21 -0600

    Added seed data to integration tests

---
+++ TodoAPIIntegrationTests/SeedData.cs
@@ -0,0 +1,18 @@
+using System;
+using Web.Api.Core.Domain.Entities;
+using Web.Api.Infrastructure.Data;
+using TodoAPI.Models;
+
+namespace Web.Api.IntegrationTests
+{
+    public static class SeedData
+    {
+        public static void PopulateTestData(AppDbContext dbContext)
+        {
+            dbContext.Todo.Add(new Todo() { Id = 1, Name = "Feed the dog", IsComplete = false });
+            dbContext.Todo.Add(new Todo() { Id = 2, Name = "Do things", IsComplete = true });
+            dbContext.Todo.Add(new Todo() { Id = 3, Name = "Do things new", IsComplete = false });
+            dbContext.SaveChanges();
+        }
+    }
+}
